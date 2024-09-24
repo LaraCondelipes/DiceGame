4db4743381431f6506f92923cc4f8fc1e3d1a378
LaraCondelipes <LaraCondelipes07@gmail.com>
2024-09-24 23:27:14 +0100

    Add project files.

---
+++ DiceGame/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DiceGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Dice Game!!!!");
+            Console.WriteLine("Introduz o nome do primeiro jogador: ");
+            var player1 = Console.ReadLine();
+            Console.WriteLine("Introduz o nome do segundo jogador: ");
+            var player2 = Console.ReadLine();
+
+            if (player1 == player2) 
+            {
+                Console.WriteLine("Os nomes não podem ser iguais.");
+
+            }
+            
+
+        }
+
+    }
+    
+}
